ed669bf9b16e5ecd62c07e3bcff999c8e3494518
romain.veltz@inria.fr <rveltz>
2022-01-23 15:41:52 +0100

    take out AbstractContinuationIterable of Continuation.jl

--- src/Continuation.jl
+++ src/Continuation.jl
@@ -1,6 +1,4 @@
 import Base: iterate
-abstract type AbstractContinuationIterable end
-abstract type AbstractContinuationState end
 ####################################################################################################
 # Iterator interface
 """
