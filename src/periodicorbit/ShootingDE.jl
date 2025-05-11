fac3dbed22c170f1fba9278d286c6ca22dc68754
romain veltz <romain.veltz@inria.fr>
2025-05-11 17:07:09 +0200

    correct generate_ci_problem(shooting::ShootingProblem

--- src/periodicorbit/ShootingDE.jl
+++ src/periodicorbit/ShootingDE.jl
@@ -240,32 +240,29 @@ function generate_ci_problem(shooting::ShootingProblem,
     t0 = sol.t[begin]
     u0 = sol(t0)
     M = shooting.M
-
-    lens = getlens(bifprob)
-    pars = getparams(bifprob)
-
+    
     # points for the sections
     centers = [copy(sol(t)) for t in LinRange(tspan[1], tspan[2], M+1)[1:end-1]]
-
+    
     # shooting kwargs
-    sh_kw = (#lens = lens, 
-            jacobian = shooting.jacobian,
-            parallel = shooting.parallel,
-            update_section_every_step = shooting.update_section_every_step,
-            )
-
+    sh_kw = (lens = getlens(bifprob), 
+    jacobian = shooting.jacobian,
+    parallel = shooting.parallel,
+    update_section_every_step = shooting.update_section_every_step,
+    )
+    
     # do we provide an ODE alg for computing the monodromy?
     if isnothing(prob_mono)
         probsh = ShootingProblem(prob_de, alg, centers; 
-                            sh_kw..., 
-                            ksh...)
+        sh_kw..., 
+        ksh...)
     else
         probsh = ShootingProblem(prob_de, alg, prob_mono, alg, centers; 
-                        sh_kw...,
-                        ksh...)
-        @info has_monodromy_DE(probsh.flow)
+        sh_kw...,
+        ksh...)
+        @info has_mono_DE(probsh.flow)
     end
-
+    
     if ~use_bordered_array
         @assert u0 isa AbstractVector
         cish = reduce(vcat, centers)
@@ -273,10 +270,8 @@ function generate_ci_problem(shooting::ShootingProblem,
     else
         cish = BorderedArray(VectorOfArray(deepcopy(centers)), tspan[2]-tspan[1])
     end
-
-    wrap_sh = WrapPOSh(probsh, nothing, nothing, pars, lens, nothing, nothing)
-
-    return wrap_sh, cish
+    
+    return probsh, cish
 end
 
 generate_ci_problem(pb::ShootingProblem, bifprob::AbstractBifurcationProblem, prob_de, sol::AbstractTimeseriesSolution, period::Real; ksh...) = generate_ci_problem(pb, bifprob, prob_de, sol, (zero(period), period); ksh...)
