2c285c7f7ca9c9d994b015a4f2af5b21133eea6c
romain veltz <romain.veltz@inria.fr>
2025-05-17 14:05:45 +0200

    for tagging a new version

--- src/codim2/MinAugHopf.jl
+++ src/codim2/MinAugHopf.jl
@@ -654,8 +654,7 @@ function test_bt_gh(iter, state)
     ζ★ ./= dot(ζ, ζ★)
     @debug "Hopf normal form computation"
     hp0 = Hopf(x, nothing, p1, ω, newpar, lens1, ζ, ζ★, (a = zero(Complex{𝒯}), b = zero(Complex{𝒯})), :hopf)
-    hp = hopf_normal_form(𝐇.prob_vf, hp0, 𝐇.linsolver; verbose = false, autodiff = false) # on met autodiff = false, on fait comment? => dans BifProblem
-
+    hp = hopf_normal_form(𝐇.prob_vf, hp0, 𝐇.linsolver; verbose = false, autodiff = false) # TODO! WE NEED A KWARGS here
     # lyapunov coefficient
     probhopf.l1 = hp.nf.b
     # test for Bautin bifurcation.
